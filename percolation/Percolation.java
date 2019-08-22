ff200a09d840282ed29d6cc36234a0134420056c
Oleksii Onishchenko <o.onishchenko@finik.pro>
2019-08-22 11:28:04 +0300

    Percolation - Initial commit

---
+++ percolation/Percolation.java
@@ -0,0 +1,41 @@
+/* *****************************************************************************
+ *  Name:              Oleksii Onishchenko
+ *  Coursera User ID:  9b3122cdaa15288f1eb7320f1480fa06
+ **************************************************************************** */
+public class Percolation {
+
+    // creates n-by-n grid, with all sites initially blocked
+    public Percolation(int n) {
+
+    }
+
+    // opens the site (row, col) if it is not open already
+    public void open(int row, int col) {
+
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        return true;
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        return true;
+    }
+
+    // returns the number of open sites
+    public int numberOfOpenSites() {
+        return 0;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return true;
+    }
+
+    // test client (optional)
+    public static void main(String[] args) {
+
+    }
+}
