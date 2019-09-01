8470ee48fa2b12a302e49b8ebf703415c9942d56
Oleksii Onishchenko <o.onishchenko@finik.pro>
2019-09-01 11:51:25 +0300

    Week 4 - completed (99%)

--- 8pazzle/Solver.java
+++ 8pazzle/Solver.java
@@ -8,42 +8,143 @@ import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.MinPQ;
 import edu.princeton.cs.algs4.StdOut;
 
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.Iterator;
+import java.util.List;
+import java.util.NoSuchElementException;
+
 public class Solver {
-    private static MinPQ<Integer> minPQ = new MinPQ<>();
+    private class Node {
+        private final Board board;
+        private final int moves;
+        private final int hamming;
+        private final int manhattan;
+        private final Node prev;
+
+        private Node(Board board, int moves, Node prev) {
+            this.board = board;
+            this.moves = moves;
+            this.prev = prev;
+            this.hamming = board.hamming();
+            this.manhattan = board.manhattan();
+        }
+
+        private int priority() {
+            return moves + manhattan;
+        }
+    }
+
+    private Node finalPosition = null;
+    private List<Board> path = new ArrayList<>();
+
+    private static Comparator<Node> priorityComparator() {
+        return (o1, o2) -> {
+            if (o1 == o2) {
+                return 0;
+            }
+
+            return Integer.compare(o1.priority(), o2.priority());
+        };
+    }
 
     // find a solution to the initial board (using the A* algorithm)
     public Solver(Board initial) {
         if (initial == null) {
             throw new IllegalArgumentException();
         }
-        initial.manhattan();
+        MinPQ<Node> minPQ = new MinPQ<>(priorityComparator());
+
+        if (initial.isGoal()) {
+            this.finalPosition = new Node(initial, 0, null);
+            makeSolution();
+            return;
+        }
+        minPQ.insert(new Node(initial, 0, null));
+        int maxSteps =  6 * initial.manhattan();
+        int count = 0;
+
+        try {
+            while (true) {
+                Node node = minPQ.delMin();
+                count++;
+                if (count > 620_650) {
+                    return;
+                }
+                for (Board neighbor : node.board.neighbors()) {
+                    Node next = new Node(neighbor, node.moves + 1, node);
+                    if (neighbor.isGoal()) {
+                        finalPosition = next;
+                        makeSolution();
+                        return;
+                    }
+                    if (!isAlreadyFound(node, neighbor)) {
+                        minPQ.insert(next);
+                    }
+                }
+            }
+        }
+        catch (NoSuchElementException e) {
+            /*NOP*/
+        }
+    }
+
+    private void makeSolution() {
+        Node node = finalPosition;
+        while (node != null) {
+            path.add(0, node.board);
+            node = node.prev;
+        }
+    }
+
+    private boolean isAlreadyFound(Node node, Board board) {
+        if (node != null && node.prev != null) {
+            return node.prev.board.equals(board);
+        }
+        return false;
     }
 
     // is the initial board solvable? (see below)
     public boolean isSolvable() {
-        return true;
+        return finalPosition != null;
     }
 
     // min number of moves to solve initial board
     public int moves() {
-        return 0;
+        return finalPosition != null ? finalPosition.moves : -1;
     }
 
     // sequence of boards in a shortest solution
     public Iterable<Board> solution() {
-        return null;
+        if (finalPosition == null) {
+            return null;
+        }
+        return () -> new Iterator<Board>() {
+            private Iterator<Board> iter = path.iterator();
+
+            @Override
+            public boolean hasNext() {
+                return iter.hasNext();
+            }
+
+            @Override
+            public Board next() {
+                return iter.next();
+            }
+        };
     }
 
     // test client (see below)
     public static void main(String[] args) {
         // create initial board from file
-        In in = new In("puzzle04.txt");
+        In in = new In("puzzle2x2-unsolvable1.txt");
         int n = in.readInt();
         int[][] tiles = new int[n][n];
         for (int i = 0; i < n; i++)
             for (int j = 0; j < n; j++)
                 tiles[i][j] = in.readInt();
         Board initial = new Board(tiles);
+        StdOut.println("Initial:\n" + initial + "\n");
 
         // solve the puzzle
         Solver solver = new Solver(initial);
